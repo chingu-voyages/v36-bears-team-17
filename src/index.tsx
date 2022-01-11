b393c46c9e5b7c2529b1bfe450413c8e62be16e8
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-01-11 07:33:12 -0800

    MUI Setup
     - MUI Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+import * as serviceWorker from './serviceWorker'
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
