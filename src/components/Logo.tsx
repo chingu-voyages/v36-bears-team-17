e10fb5171a01732ebe84693562f852216afe69bb
Josue Rodriguez <maybejosue@gmail.com>
2022-02-02 15:07:58 -0800

    create logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from "react";
+import { Typography, Box } from "@mui/material";
+import { useNavigate } from "react-router";
+
+export default function Logo(): ReactElement {
+  const navigate = useNavigate();
+  return (
+    <Box
+      sx={{
+        m: "2rem 0",
+        display: "flex",
+        justifyContent: "center",
+        cursor: "pointer",
+      }}
+      onClick={() => navigate("/home")}
+    >
+      <Typography>Icon and Logo name</Typography>
+    </Box>
+  );
+}
